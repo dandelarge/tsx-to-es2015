3486f0bd9d97a679803329163de5c75582e053a6
Sergio Daniel <sergio-daniel@backbase.com>
2021-01-28 14:31:40 +0100

    Small app with a single component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 function App() {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, [])
+
+  const transpileInputCode = async (input: string) => {
+
+    const transpiled = await ref.current.transform(input, {
+      loader: 'tsx',
+      target: 'es2015'
+    });
+
+    setCode(transpiled.code);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lame, ugly but functional web transpiler!</h2>
+      <textarea
+        onChange={ e => {
+          setInput(e.target.value);
+          transpileInputCode(e.target.value);
+         } }
+        value={ input }
+        name="inputCode"
+        id="inputCode"
+        cols={120} rows={10}></textarea>
+      <input
+        onClick={ () => transpileInputCode(input)}
+        type="button"
+        value="Transpile!"/>
+      <pre id="transpiledCode">{ code }</pre>
     </div>
   );
 }
